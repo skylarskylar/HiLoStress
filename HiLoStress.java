a679dd62f03147eb02ebce5fdc0f24508d1e2bda
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-11-20 11:06:25 -0800

    Update HiLoStress.java

--- HiLoStress.java
+++ HiLoStress.java
@@ -5,6 +5,9 @@ public class HiLoStress {
 	public static void main (String [] args) throws FileNotFoundException 
 	{
 		Scanner s = new Scanner (new File(fileName));
+		int day = scan.nextInt();
+		int [] lows = new int [day];
+		int[] highs = new int [day];
 	}
 	
 }
