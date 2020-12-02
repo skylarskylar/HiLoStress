df4c536c107498dc374a58d320825266d642bc18
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-12-02 01:07:36 -0800

    Update HiLoStress.java

--- HiLoStress.java
+++ HiLoStress.java
@@ -1,13 +1,41 @@
 import java.util.*;
 
+
 public class HiLoStress {
 
 	public static void main (String [] args) throws FileNotFoundException 
 	{
-		Scanner s = new Scanner (new File(fileName));
-		int day = scan.nextInt();
-		int [] lows = new int [day];
-		int[] highs = new int [day];
+		final String FILENAME = "jobs.txt";
+		
+		
+
+		Scanner s = new Scanner (new File(FILENAME));
+		int ndays = s.nextInt();
+		int [] loStress = new int [ndays];
+		int[] hiStress = new int [ndays];
+		
+		for (int i=0; i<ndays; i++) // setting up the arrays for each inputting info
+		{
+			loStress[i] = s.nextInt();
+			hiStress[i] = s.nextInt();
+		}
+		
+		public int maxMoney (int days, int[] lo, int[] hi)
+		{
+		
+		if (days == 0)
+		{
+			return lo[0];
+		}
+		if (days == 1)
+		{
+			return Math.max(lo[0] + lo[1], hi[1]);
+		}
+		
+		return Math.max(lo[ndays]+ maxMoney(days-1, hi, lo), hi[days]+maxMoney(days-2, hi, lo))
+		}
+		
+		return maxMoney (ndays-1, lowStress, hiStress);
 	}
 	
 }
